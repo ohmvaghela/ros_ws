9d91c075d0d78e24a34d2dad77d57d49c5a63a34
ohm <vaghelaohm@gmail.com>
2023-01-23 13:03:28 +0530

    first commit

---
+++ ros_arduino/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/ohm/GithubRepos/ros_ws/ros_arduino/src
+# Build directory: /home/ohm/GithubRepos/ros_ws/ros_arduino/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ ros_arduino/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/ohm/GithubRepos/ros_ws/ros_arduino/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
