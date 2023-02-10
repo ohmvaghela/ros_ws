f68337f3b28e0c591caca71b502545147fb2f920
vaghelaohm <vaghelaohm@gmail.com>
2023-02-10 21:59:16 +0530

    slider pubsub

--- ros_arduino_ws/src/exo_angle_control/src/anglePubSub.cpp
+++ ros_arduino_ws/src/exo_angle_control/src/anglePubSub.cpp
@@ -8,20 +8,19 @@
  * This tutorial demonstrates simple sending of messages over the ROS system.
  */
 
-    int hipLeft = 50;
-    int hipRight = 50;
-    int kneeLeft = 50;
-    int kneeRight = 50;
+int hipLeft = 50;
+int hipRight = 50;
+int kneeLeft = 50;
+int kneeRight = 50;
 
-// void chatterCallback(const exo_angle_control::ExoAngleChange &msg)
-// {
-//     ROS_INFO("I heard: [%i], [%i], [%i], [%i]", msg.hipLeft, msg.kneeRight, msg.kneeRight, msg.hipRight);
-//     hipLeft = msg.hipLeft;
-//     hipRight = msg.hipRight;
-//     kneeLeft = msg.kneeLeft;
-//     kneeRight = msg.kneeRight;
-// }
-
+void chatterCallback(const exo_angle_control::ExoAngleChange &msg)
+{
+    ROS_INFO("I heard: [%i], [%i], [%i], [%i]", msg.hipLeft, msg.kneeRight, msg.kneeRight, msg.hipRight);
+    hipLeft = msg.hipLeft;
+    hipRight = msg.hipRight;
+    kneeLeft = msg.kneeLeft;
+    kneeRight = msg.kneeRight;
+}
 
 int main(int argc, char **argv)
 {
@@ -30,16 +29,14 @@ int main(int argc, char **argv)
 
     ros::NodeHandle n;
 
-
     // publisher
     ros::Publisher angle_pub = n.advertise<exo_angle_control::ExoAngle>("desiredAngleTopic", 1000);
 
     // subscriber
-    // ros::Subscriber sub = n.subscribe("desiredAngleTopic", 1000, chatterCallback);
+    ros::Subscriber sub = n.subscribe("updateAngleTopic", 1000, chatterCallback);
 
     // genreal
     ros::Rate loop_rate(10);
-
 
     while (ros::ok())
     {
@@ -50,15 +47,14 @@ int main(int argc, char **argv)
         // ROS_INFO("%i", pub_angle.des_angle);
 
         exo_angle_control::ExoAngle des_angles;
-        
+
         des_angles.hipLeft = hipLeft;
         des_angles.hipRight = hipRight;
         des_angles.kneeLeft = kneeLeft;
         des_angles.kneeRight = kneeRight;
 
         angle_pub.publish(des_angles);
-        ROS_INFO("pub %i | %i | %i | %i ", des_angles.hipLeft, des_angles.hipRight, des_angles.kneeLeft,
-                 des_angles.kneeRight);
+        ROS_INFO("pubsub %i | %i | %i | %i ", des_angles.hipLeft, des_angles.hipRight, des_angles.kneeLeft, des_angles.kneeRight);
 
         ros::spinOnce();
 
