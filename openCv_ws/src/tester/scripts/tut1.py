3b6eed8a1f5dd5c0673db48833fa4d83f0b47945
ohmExoSkeleton <ohmExoSkeleton@gmail.com>
2023-04-11 12:21:06 +0530

    openjcv lab

--- openCv_ws/src/tester/scripts/tut1.py
+++ openCv_ws/src/tester/scripts/tut1.py
@@ -1,22 +1,28 @@
 import cv2
 import sys
 import numpy as np
-logo_path = "/home/ohm/GitRepo/ros_ws/openCv_ws/src/tester/images/logo.png"
-rand_path = "/home/ohm/GitRepo/ros_ws/openCv_ws/src/tester/images/random.jpg"
+logo_path = "/home/exoskeleton/GitHubRepo/ros_ws/openCv_ws/src/tester/images/logo.png"
+rand_path = "/home/exoskeleton/GitHubRepo/ros_ws/openCv_ws/src/tester/images/random.jpg"
+cat_path =  "/home/exoskeleton/GitHubRepo/ros_ws/openCv_ws/src/tester/images/cat.jpeg"
+tree_path =  "/home/exoskeleton/GitHubRepo/ros_ws/openCv_ws/src/tester/images/tree.jpg"
+
 logo = cv2.imread(logo_path, 1)
 rnad = cv2.imread(rand_path, 1)
-W=480
-thickness = 2
-line_type = 8
-cv2.circle(logo,(300,300),100,(0,0,0),15,lineType=cv2.FILLED)
-blend1 = cv2.addWeighted(logo, 0.5, rnad, 0.4, -25)
-blend2 = cv2.addWeighted(logo, 0.5, rnad, 0.4, 10)
-cv2.putText(logo,"ohm vaghela",(100,600),cv2.FONT_HERSHEY_TRIPLEX,2,(0,0,0),2,cv2.LINE_4)
-cv2.imshow('logo',logo)
-# cv2.imshow('RANd',rnad)
-# cv2.imshow('blend1', blend1)
-# cv2.imshow('blend2', blend2)
+cat = cv2.imread(cat_path,1)
+tree =cv2.imread(tree_path,1)
 
+h,w = tree.shape[:2]
+tree = cv2.resize(tree,(w//3,h//3))
+
+blur = cv2.blur(tree,(7,7))
+Gblur = cv2.GaussianBlur(tree,(7,7),0,0)
+BLF = cv2.bilateralFilter(tree,100,80,80)
+medianBlur = cv2.medianBlur(tree,3)
+cv2.imshow("cat",tree)
+# cv2.imshow('blur',blur)
+cv2.imshow("gausian",Gblur)
+cv2.imshow("BLF",BLF)
+cv2.imshow("mblur",medianBlur)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
