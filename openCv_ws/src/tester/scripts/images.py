d7e02ae462c60d17ec1a4b235170216d15d3b2e0
ohmExoSkeleton <ohmExoSkeleton@gmail.com>
2023-04-13 16:16:19 +0530

    lab idk

--- openCv_ws/src/tester/scripts/images.py
+++ openCv_ws/src/tester/scripts/images.py
@@ -8,13 +8,16 @@ noisy_path =  os.path.join(os.environ['GIT_REPO_PATH'], "ros_ws/openCv_ws/src/te
 lines_path =  os.path.join(os.environ['GIT_REPO_PATH'], "ros_ws/openCv_ws/src/tester/images/lines.jpg")
 lines1_path =  os.path.join(os.environ['GIT_REPO_PATH'], "ros_ws/openCv_ws/src/tester/images/lines1.jpg")
 tones_path =  os.path.join(os.environ['GIT_REPO_PATH'], "ros_ws/openCv_ws/src/tester/images/tones.png")
+color_balls_path =  os.path.join(os.environ['GIT_REPO_PATH'], "ros_ws/openCv_ws/src/tester/images/color_balls.webp")
 
-logo = cv2.imread(logo_path, 0)
+
+logo = cv2.imread(logo_path, 1)
 rnad = cv2.imread(rand_path, 1)
 cat = cv2.imread(cat_path,1)
-tree =cv2.imread(tree_path,0)
+tree =cv2.imread(tree_path,1)
 noisy = cv2.imread(noisy_path,1)
 lines = cv2.imread(lines_path,1)
-lines1 = cv2.imread(lines1_path,0)
+lines1 = cv2.imread(lines1_path,1)
 tones = cv2.imread(tones_path,1)
+color_balls = cv2.imread(color_balls_path,1)
 
--- openCv_ws/src/tester/scripts/tut1.py
+++ openCv_ws/src/tester/scripts/tut1.py
@@ -2,26 +2,28 @@ import cv2
 import sys
 import numpy as np
 import os
-from images import tones,tree, lines
-my_img = np.copy(tones)
+from images import tones,tree, lines,color_balls
+my_img = np.copy(color_balls)
+my_img = cv2.resize(my_img,(my_img.shape[1]//2,my_img.shape[0]//2))
+my_img_grey = cv2.cvtColor(my_img,cv2.COLOR_BGR2GRAY)
+# cv2.inRange(miaf,)
 
-def nothing(x):
-    pass
+max_value = 255
+max_value_H = 360//2
+low_H = 100
+low_S = 100
+low_V = 100
+high_H = max_value_H
+high_S = max_value
+high_V = max_value
 
-cv2.namedWindow("erode")
-cv2.createTrackbar("val","erode",1,10,nothing) 
-cv2.imshow("img",my_img) 
-erode = cv2.erode(my_img,(3,3))
-cv2.waitKey(0)
-while True:
-    cv2.imshow("erode",erode)
+frame = my_img
+frame_HSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+frame_threshold = cv2.inRange(frame_HSV, (low_H, low_S, low_V), (high_H, high_S, high_V))
+    
+_,thresh = cv2.threshold(my_img_grey,100,255,type=cv2.THRESH_TRUNC)
+cv2.imshow("img1",frame_threshold)
 
-    k = cv2.waitKey(1) & 0xFF
-    if k == 27:
-        break
-    k_size = cv2.getTrackbarPos('val','erode')
-    k_size_odd = (k_size*2) + 1
-    print(k_size_odd)
-    erode = cv2.erode(erode,(k_size_odd,k_size_odd))
+cv2.waitKey()
 cv2.destroyAllWindows()
 
