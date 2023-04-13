92e1eab3b321cc7b5b87999f37ed2f59508f9876
vaghelaohm <vaghelaohm@gmail.com>
2023-04-13 09:40:23 +0530

    opencv hostel

--- openCv_ws/src/tester/scripts/tut1.py
+++ openCv_ws/src/tester/scripts/tut1.py
@@ -1,28 +1,22 @@
 import cv2
 import sys
 import numpy as np
-logo_path = "/home/exoskeleton/GitHubRepo/ros_ws/openCv_ws/src/tester/images/logo.png"
-rand_path = "/home/exoskeleton/GitHubRepo/ros_ws/openCv_ws/src/tester/images/random.jpg"
-cat_path =  "/home/exoskeleton/GitHubRepo/ros_ws/openCv_ws/src/tester/images/cat.jpeg"
-tree_path =  "/home/exoskeleton/GitHubRepo/ros_ws/openCv_ws/src/tester/images/tree.jpg"
+logo_path = "/home/ohm/GitRepo/ros_ws/openCv_ws/src/tester/images/logo.png"
+rand_path = "/home/ohm/GitRepo/ros_ws/openCv_ws/src/tester/images/random.jpg"
+cat_path =  "/home/ohm/GitRepo/ros_ws/openCv_ws/src/tester/images/cat.jpeg"
+tree_path =  "/home/ohm/GitRepo/ros_ws/openCv_ws/src/tester/images/tree.jpg"
+noisy_path =  "/home/ohm/GitRepo/ros_ws/openCv_ws/src/tester/images/Noisy_image.png"
 
-logo = cv2.imread(logo_path, 1)
+logo = cv2.imread(logo_path, 0)
 rnad = cv2.imread(rand_path, 1)
 cat = cv2.imread(cat_path,1)
-tree =cv2.imread(tree_path,1)
+tree =cv2.imread(tree_path,0)
 
 h,w = tree.shape[:2]
-tree = cv2.resize(tree,(w//3,h//3))
+tree = cv2.resize(tree,(w//2,h//2))
+hitmiss = cv2.morphologyEx(logo,cv2.MORPH_HITMISS,(17,17))
 
-blur = cv2.blur(tree,(7,7))
-Gblur = cv2.GaussianBlur(tree,(7,7),0,0)
-BLF = cv2.bilateralFilter(tree,100,80,80)
-medianBlur = cv2.medianBlur(tree,3)
-cv2.imshow("cat",tree)
-# cv2.imshow('blur',blur)
-cv2.imshow("gausian",Gblur)
-cv2.imshow("BLF",BLF)
-cv2.imshow("mblur",medianBlur)
+cv2.imshow("hitmiss",hitmiss)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
