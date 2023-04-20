49506c477baa46797ef2e052b75bee0a36a4c45a
vaghelaohm <vaghelaohm@gmail.com>
2023-04-20 14:31:48 +0530

    asdas

--- openCv_ws/src/tester/scripts/tut1.py
+++ openCv_ws/src/tester/scripts/tut1.py
@@ -3,27 +3,19 @@ import sys
 import numpy as np
 import os
 from images import tones,tree, lines,color_balls
-my_img = np.copy(color_balls)
-my_img = cv2.resize(my_img,(my_img.shape[1]//2,my_img.shape[0]//2))
-my_img_grey = cv2.cvtColor(my_img,cv2.COLOR_BGR2GRAY)
-# cv2.inRange(miaf,)
 
-max_value = 255
-max_value_H = 360//2
-low_H = 100
-low_S = 100
-low_V = 100
-high_H = max_value_H
-high_S = max_value
-high_V = max_value
-
-frame = my_img
-frame_HSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-frame_threshold = cv2.inRange(frame_HSV, (low_H, low_S, low_V), (high_H, high_S, high_V))
+def singleDigit(x):
+    temp = 0
+    while True:
+        while(x!=0):
+            temp = temp + x%10 # 8
+            x = x//10 # 4
+        if(temp<10): return temp
+        x = temp
+        temp = 0
     
-_,thresh = cv2.threshold(my_img_grey,100,255,type=cv2.THRESH_TRUNC)
-cv2.imshow("img1",frame_threshold)
-
-cv2.waitKey()
-cv2.destroyAllWindows()
+    
+digit = 45
+# singleDigit(digit)
+print(singleDigit(digit))
 
--- ros_arduino_ws/src/exo_angle_control/src/ros_control/hardware_if.cpp
+++ ros_arduino_ws/src/exo_angle_control/src/ros_control/hardware_if.cpp
@@ -11,8 +11,6 @@
 #include "exo_angle_control/EncoderHR.h"
 #include "exo_angle_control/ExoAngle.h"
 
-
-
 class BLDCRobot : public hardware_interface::RobotHW
 {
 public:
@@ -26,9 +24,7 @@ public:
       joint_efforts_[i] = 0.0;
       joint_position_interface_.registerHandle(hardware_interface::JointHandle(
           "bldc_joint_" + std::to_string(i),
-          &joint_positions_[i],
-          &joint_velocities_[i],
-          &joint_efforts_[i]));
+          &joint_positions_[i]));
     }
     registerInterface(&joint_position_interface_);
 
@@ -42,71 +38,43 @@ public:
     }
     registerInterface(&joint_command_interface_);
   }
-    void hlCallback(const exo_angle_control::EncoderHL::ConstPtr& msg)
-    {
-        hl_angle_ = msg->angle;
-    }
-    
-    void klCallback(const exo_angle_control::EncoderKL::ConstPtr& msg)
-    {
-        kl_angle_ = msg->angle;
-    }
-    
-    void krCallback(const exo_angle_control::EncoderKR::ConstPtr& msg)
-    {
-        kr_angle_ = msg->angle;
-    }
-    
-    void hrCallback(const exo_angle_control::EncoderHR::ConstPtr& msg)
-    {
-        hr_angle_ = msg->angle;
-    }
+  void hlCallback(const exo_angle_control::EncoderHL::ConstPtr &msg) {hl_angle_ = msg->angle;}
+  void klCallback(const exo_angle_control::EncoderKL::ConstPtr &msg) {kl_angle_ = msg->angle;}
+  void krCallback(const exo_angle_control::EncoderKR::ConstPtr &msg) {kr_angle_ = msg->angle;}
+  void hrCallback(const exo_angle_control::EncoderHR::ConstPtr &msg) {hr_angle_ = msg->angle;}
 
-    void read(ros::Time time, ros::Duration period)
-    {
-        ros::spinOnce(); // Process all incoming messages
-    }
- ros::Time get_time() const
-  {
-    return ros::Time::now();
-  }
-
-  ros::Duration get_period() const
-  {
-    return ros::Duration(1.0 / CONTROL_FREQUENCY);
-  }
-  void write()
+  void read(ros::Time time, ros::Duration period) {ros::spinOnce();}
+  // Send duty cycle command to the motor driver
+  // The duty cycle value should be between -1.0 and 1.0
+  void write() 
   {
     for (int i = 0; i < 4; ++i)
-    {
-      // Send duty cycle command to the motor driver
-      // The duty cycle value should be between -1.0 and 1.0
       send_duty_cycle_command(i, joint_commands_[i]);
-    }
   }
+  
 
-    
-    void send_duty_cycle_command()
-    {
-        exo_angle_control::ExoAngle msg;
-        msg.hipLeft = hip_left_duty_cycle_;
-        msg.hipRight = hip_right_duty_cycle_;
-        msg.kneeLeft = knee_left_duty_cycle_;
-        msg.kneeRight = knee_right_duty_cycle_;
-        desired_angle_pub_.publish(msg);
-    }
-    ros::Publisher desired_angle_pub_;
+  void send_duty_cycle_command()
+  {
+    exo_angle_control::ExoAngle msg;
+    msg.hipLeft = hip_left_duty_cycle_;
+    msg.hipRight = hip_right_duty_cycle_;
+    msg.kneeLeft = knee_left_duty_cycle_;
+    msg.kneeRight = knee_right_duty_cycle_;
+    desired_angle_pub_.publish(msg);
+  }
+  ros::Publisher desired_angle_pub_;
+
 private:
-   double hip_left_duty_cycle_;
-    double hip_right_duty_cycle_;
-    double knee_left_duty_cycle_;
-    double knee_right_duty_cycle_;
-    
-    // Publisher for the desired angle message
-    uint32_t hl_angle_;
-    uint32_t kl_angle_;
-    uint32_t kr_angle_;
-    uint32_t hr_angle_;
+  double hip_left_duty_cycle_;
+  double hip_right_duty_cycle_;
+  double knee_left_duty_cycle_;
+  double knee_right_duty_cycle_;
+
+  // Publisher for the desired angle message
+  uint32_t hl_angle_;
+  uint32_t kl_angle_;
+  uint32_t kr_angle_;
+  uint32_t hr_angle_;
   double joint_positions_[4];
   double joint_velocities_[4];
   double joint_efforts_[4];
@@ -116,16 +84,16 @@ private:
   hardware_interface::PositionJointInterface joint_command_interface_;
 };
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
   ros::init(argc, argv, "bldc_robot_hw_interface");
   ros::NodeHandle nh;
-    
+
   BLDCRobot robot;
-robot.desired_angle_pub_ = nh.advertise<exo_angle_control::ExoAngle>("desiredAngleTopic", 1);
+  robot.desired_angle_pub_ = nh.advertise<exo_angle_control::ExoAngle>("desiredAngleTopic", 1);
 
   // Register the hardware interface with the controller manager
-  hardware_interface::RobotHW* robot_ptr = &robot;
+  hardware_interface::RobotHW *robot_ptr = &robot;
   controller_manager::ControllerManager cm(robot_ptr, nh);
   cm.loadController("my_controller");
   cm.switchController("my_controller");
@@ -134,7 +102,7 @@ robot.desired_angle_pub_ = nh.advertise<exo_angle_control::ExoAngle>("desiredAng
   while (ros::ok())
   {
     robot.read();
-    cm.update(robot.get_time(), robot.get_period());
+    cm.update(ros::Time::now(), ros::Duration(0.02));
     robot.write();
     loop_rate.sleep();
   }
